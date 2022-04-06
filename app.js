9d929bc22eed8f7561ea63da426201fc7e2df2c2
Annalisa De Martino <lizmartinmusic@gmail.com>
2022-04-06 11:47:41 +0200

    second commit

--- app.js
+++ app.js
@@ -2,8 +2,24 @@ const express = require('express');
 const app = express();
 
 
-app.get('/', (req, res) => {
-  res.send("Hello user!");
+app.get('/tasks', (req, res) => {
+  res.send("Checking all tasks");
+});
+
+app.get('/tasks/:id', (req, res) => {
+  res.send("Checking tasks by id");
+});
+
+app.get('/tasks/:status', (req, res) => {
+  res.send("Checking tasks by status");
+});
+
+app.post('/tasks', (req, res) => {
+  res.send("Posting task");
+});
+
+app.delete('/tasks(:id', (req, res) => {
+  res.send("Posting task");
 });
 
 const port = 3000;
