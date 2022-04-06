5eab75ab86962378629bfb1fa999b165fd5dbfcf
Annalisa De Martino <lizmartinmusic@gmail.com>
2022-04-06 17:33:17 +0200

    adding items to list and little css style

--- app.js
+++ app.js
@@ -4,29 +4,25 @@ const ejs = require('ejs');
 
 const app = express(); //creating server
 
-let items = []; //create an empty array for the list items
+const items = []; //create an empty array for the list items
 
 //some stuff I dunno
 app.use(express.static('public')); //set where my public files are
-app.set('view engine', 'ejs');
-app.use(express.urlencoded({extended: true}));
-app.use(express.json());
+app.set('view engine', 'ejs');   //to read ejs file in views folder
+app.use(express.urlencoded({extended: true}));  //substitute of body parser
+app.use(express.json()); //substitute of body parser
 
 
-//when I load the homepage, load index.ejs
+//when I load the root '/', load index.ejs (with ejs we have to use res.render)
 app.get('/', (req, res) => {
-  res.render('index', {newListItems: items}); //'index' means index.ejs page
+  res.render('index', {newListItems: items}); //'index' means index.ejs page, newListItems is name of the array in ejs page while items is the name of array in app.js
 });
 
 //when I press submit button, parse newItem value and assign to item var, then push item inside items array
 app.post('/', (req, res) => {
-  let item = req.body.newItem;
-  items.push(item);
-  console.log(item);
+  items.push(req.body.newItem); //push inside array items the value of form input (= newItem)
   res.redirect('/');
 });
-
-
 
 
 // SERVER RUNNING ON PORT 3000...
