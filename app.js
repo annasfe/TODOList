b93591294ae2dc61b245779f8321ea9e32386664
Vivi Ya <viviviyanez@gmail.com>
2022-04-07 12:36:33 +0200

    array of tasks included and mostly working

--- app.js
+++ app.js
@@ -1,40 +1,82 @@
-const express = require('express')
-const app = express()
-const hostname = 'localhost';
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+const portName = 'localhost';
 const port = process.env.PORT || 3000;
 
 
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
-  })
 
 
-app.route('/tasks')
+//tasks array
+const allTheTasks = [];
+
+//task creator 
+class TaskCreator {
+    constructor (id="", content="One task", category="", status="to do"){
+        this.id = id;
+        this.content = content;
+        this.category = category;
+        this.status = status;
+    }
+}
+
+const task = new TaskCreator(1, 'first task', 'home', 'to do');
+allTheTasks.push(task);
+
+app.route('/api/tasks')
     .get((req, res) => {
-    res.send(`<html> <head> <title> Todo List </title> </head> <body><h1>All the tasks</h1>`)
-})
-
+      res.send(`These are all your tasks: ${getTaskContent(allTheTasks)}`);
+      console.log(allTheTasks);
+   })
     .post((req, res) => {
-    console.log(req.body.task)
-    res.send('New task received, thanks!');
+      let newId = allTheTasks.length+1;
+      const task = new TaskCreator(newId, req.body.content, req.body.category, req.body.status);
+    
+      allTheTasks.push(task);
+      res.send(`Task added!`);
+      
  })
 
 
- 
- app.route('/tasks/:id')
- 
-    .get((req, res) => {
-    let id = req.params.id
-    res.send(`<html> <head> <title> Todo List</title> </head> <body><h1>/This is task ${id}</h1>`)
- })
+app.route('/api/tasks/:id')
+   .get((req, res) => {
+      const reqTask = allTheTasks.filter(item => item.id === parseInt(req.params.id));
+      if(reqTask != false) {res.send(`This is task ${req.params.id}: ${reqTask[0]}`);}
+      else {res.send(`There is not a task with ${req.params.id} id.`);}
+    
+})
     .put((req, res) => {
-    let id = req.params.id
-    res.send(`<html> <head> <title> Todo List</title> </head> <body><h1>/Modified task ${id}</h1>`)
+      const reqTask = allTheTasks.filter(item => item.id === parseInt(req.params.id));
+      if(reqTask != false) {
+         reqTask[0].content = req.body.content;
+         reqTask[0].category = req.body.category;
+         reqTask[0].status = req.body.status;
+         res.send(`Task ${req.params.id} changed to: ${reqTask[0]}`);
+      } else {res.send(`There is not a task with ${req.params.id} id.`);}
+    
  })
  
     .delete((req, res) => {
-    let id = req.params.id
-    res.send(`<html> <head> <title> Todo List</title> </head> <body><h1>/Deleted task ${id}</h1>`)
-     })
+      const reqTask = allTheTasks.filter(item => item.id === parseInt(req.params.id));
+      if(reqTask != false) {
+         allTheTasks.splice(allTheTasks.indexOf(reqTask),1);
+         res.send(`Task ${req.params.id} deleted!`);
+      } else {res.send(`There is not a task with ${req.params.id} id.`);}
+}) 
 
 
+function getTaskContent(arr) {
+   let allTask = [];
+   for (let i = 0; i < arr.length; i++) {
+      allTask.push(arr[i].content);
+   }
+   return allTask;
+}
+
+
+//////
+app.listen(port, (err) => {
+      if (err) console.log(err);
+      console.log(`Server running on port ${port}...`);
+   })
