b1a3ba1f6d78ce940b341db6eccdb76f95b2572a
Annalisa De Martino <lizmartinmusic@gmail.com>
2022-04-07 09:46:03 +0200

    small changes

--- app.js
+++ app.js
@@ -6,9 +6,8 @@ const app = express(); //creating server
 
 const items = []; //create an empty array for the list items
 
-//some stuff I dunno
-app.use(express.static('public')); //set where my public files are
 app.set('view engine', 'ejs');   //to read ejs file in views folder
+app.use(express.static('public')); //set where my public files are
 app.use(express.urlencoded({extended: true}));  //substitute of body parser
 app.use(express.json()); //substitute of body parser
 
@@ -18,7 +17,7 @@ app.get('/', (req, res) => {
   res.render('index', {newListItems: items}); //'index' means index.ejs page, newListItems is name of the array in ejs page while items is the name of array in app.js
 });
 
-//when I press submit button, parse newItem value and assign to item var, then push item inside items array
+//when I press submit button, parse newItem value and assign to item var, then push item inside items array and reload root page "/"
 app.post('/', (req, res) => {
   items.push(req.body.newItem); //push inside array items the value of form input (= newItem)
   res.redirect('/');
@@ -37,3 +36,5 @@ app.listen(port, hostname, (err) => {
     console.log(`Server running on port ${port}...`)
   }
 })
+
+
