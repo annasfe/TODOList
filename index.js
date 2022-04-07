05a9213b5fb2db3c09a581d30be56a0f775af858
Marie-Lyse Charriere <marielysech@gmail.com>
2022-04-07 17:24:11 +0200

    storing data in JSON

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-const express = require('express')
-const app = express()
-let ejs = require('ejs');
+const express = require('express');
+const app = express();
+const path = require('path');
+const fs = require('fs');
+const ejs = require('ejs');
+
+app.use(express.static('public'));
 app.use(express.json())
+app.set('view engine', 'ejs')
 
 const port = 3000
 const hostName = 'localhost'
@@ -13,72 +18,90 @@ app.listen(port, hostName, (err) => {
 
 
 //Database
+// let taskDatabaseJSON = [] 
+const taskDatabaseJSON = fs.readFileSync('public/storage.json')
+const taskJson = JSON.parse(taskDatabaseJSON) // convert in JSON format
 
-let taskDatabase = []; //will include all typicalEntries
-let idCounter = 0 // to assign IDs
 
 // let typicalEntry = {
-//    id: //number for each new entry it will be = counter +1
+//    id: //number for each new entry it will be = Date.now() to have unique IDs
 //    action: //plain text of the task
 //    status: //automatically set to TODO but with further action (PUT) could be modify
 //}
 
-//TODO : 
-    //Get /tasks => display all task entry in taskDatabase
-    //Post /tasks => create typicalEntry with the correct data to assign to each property of the typical object entry and push it to taskDatabase
-    //Delete /tasks => remove typicalEntry from the taskDatabase
 
-//TODO : at some point use JSON or to stringify ?
+// '/' endpoint
 
-//TODO : add verification to ensure that id comparaison is on the same format (two numbers)
-
+app.get('/', (req,res) => {
+    // res.render('index', { name: "ML" });
+})
 
 // '/tasks/' endpoint
 app.route('/tasks')
-    .get((req, res) => {
-        res.send(taskDatabase);
-    }) //access all tasks
+    .get((req, res) => { //access all tasks
+        if (taskJson) {
+            console.log(taskJson)
+            return res.send(taskJson)
+        } return res.send("No task yet")
+    // res.render('index', { tasksArray: taskDatabase });
+    })
 
-    .post((req, res) => {
+    .post((req, res) => { //create new task
         let newEntry = {
-            id: idCounter+1,
+            id: Date.now(),
             action: req.body.action,
             status: "todo"
         }
-
-        taskDatabase.push(newEntry)
-        res.redirect('/tasks');
-    }); //create new task
-
+        taskJson.push(newEntry);
+        const newData = JSON.stringify(taskJson) //back to row format
+        fs.writeFile("public/storage.json", newData, (err) => {
+            if (err) throw err;
+            console.log("New data added");
+          });
+        res.status(200)  
+        res.send(`${newEntry.action} added`);
+});
 
 // '/tasks/:id' endpoint
-app.route('/tasks/:id')
-    .get((req, res) => {
-        res.send(taskDatabase.filter(element => element.id === req.params.id));
-    }) //access specific tasks
 
-    .put((req, res) => {
-        let index = taskDatabase.findIndex( element => element.id === req.params.id)
-        taskDatabase[index].status = req.body.status
-        taskDatabase[index].action = req.body.action
+//TO DO FIX THE GET FOR '/tasks/:id'
+app.route('/tasks/:id')
+    .get((req, res) => { //access specific tasks
+        
+        const task = taskJson.filter(element => element.id === parseInt(req.params.id));
+        console.log(task)
+        res.send(task);
+    }) 
+
+    .put((req, res) => { //update specific tasks
+
+        let index = taskJson.findIndex( element => element.id === parseInt(req.params.id))
+
+        taskJson[index].status = req.body.status || taskJson[index].status
+        taskJson[index].action = req.body.action || taskJson[index].action
         res.send(`Task ${req.params.id} has been updated`);
 
-    }) //update specific tasks
+    }) 
 
-    .delete((req, res) => {
-        let index = taskDatabase.findIndex( element => element.id === req.params.id)
-        taskDatabase.splice(index, 1)
-        res.send(`Task ${req.params.id} has been deleted`);
-    }); //supress specific tasks
+    .delete((req, res) => { //supress specific tasks
+        let index = taskJson.findIndex( element => element.id === parseInt(req.params.id))
+        if (index) {
+            taskJson.splice(index, 1)
+            return res.send(`Task ${req.params.id} has been deleted`);
+        } return res.send("This task doesn't exist")
+    }); 
 
-// /tasks/:status
-app.delete('/tasks/:status', (req, res) => {
-    taskDatabase.forEach(function(elem) {
-        if(elem.status === req.params.status) {
-            taskDatabase.splice(taskDatabase.indexOf(elem), 1);
-        }
-    })    
+    //TODO check later if i can change that
+// // /tasks/:status
+// app.delete('/tasks/:status', ((req, res) => { // remove all tasks from specific status
+//     let filterArray = taskDatabase.filter((element) => {toString(element.status) !== toString(req.params.status)})
+//     taskDatabase = filterArray
+    
+//     // taskDatabase.forEach(function(elem) {
+//     //     if(elem.status === req.query.status) {
+//     //         taskDatabase.splice(taskDatabase.indexOf(elem), 1);
+//     //     }
+//     // })    
 
-    res.send(`All ${req.params.status} have been removed from the list`);
-}); // remove all tasks from specific status
-
+//     return res.send(`All ${req.params.status} have been removed from the list`);
+// })); 
