6d52ca970b24946c10fa3d98de5c81b732ab22d3
Annalisa De Martino <lizmartinmusic@gmail.com>
2022-04-07 15:02:15 +0200

    small changes

--- app.js
+++ app.js
@@ -4,29 +4,28 @@ const fs = require('fs');
 
 const app = express();
 
-let items = [];
-
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+let items = [];
+
 app.get('/tasks', (req, res) => {
   items = fs.readFileSync('todolist.txt').toString().split("\n");
-  res.render('index', { newListItems: items}); 
+  res.render('index', { newListItems: items });
 });
 
 app.post('/tasks', (req, res) => {
   items.push(req.body.newItem);
-  fs.appendFile('./todolist.txt', items[items.length-1] + "\n", 'utf8', function (err) {
-      if (err) return console.log(err);
+  fs.appendFile('./todolist.txt', items[items.length - 1] + "\n", 'utf8', function (err) {
+    if (err) return console.log(err);
   });
   res.redirect('/tasks');
 });
 
 
-
-//
+// SERVER RUNNING
 const port = process.env.PORT || 3000;
 const hostname = 'localhost';
 
